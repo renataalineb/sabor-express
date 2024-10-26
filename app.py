c73b07d84f48c071ed422ff3064f1af4ea8a8fab
renataalineb <renataalinebv@gmail.com>
2024-10-26 16:38:58 -0300

    uso de dicionários e atualização de cadastro

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 
-restaurantes = ['Pizza', 'Sushi']
+restaurantes = [{'nome': 'Praça', 'categoria': 'Japonesa', 'ativo':False},
+                {'nome':'Pizza Suprema', 'categoria': 'Italiana', 'ativo':True},
+                {'nome': 'Cantina', 'categoria':'Japonesa', 'ativo':False}]
 
 def exibir_nome_do_programa():
     print("""""
@@ -38,7 +40,9 @@ def exibir_subtitulo(texto):
 def cadastrar_novo_restaurante():
     exibir_subtitulo('Cadastro de novos restaurantes') 
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
-    restaurantes.append(nome_do_restaurante)
+    categoria = input(f'Digite o nome da categoria do restaurante {nome_do_restaurante}: ')
+    dados_do_restaurante = {'nome': nome_do_restaurante, 'categoria':categoria, 'ativo': False}
+    restaurantes.append(dados_do_restaurante)
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
     voltar_ao_menu_principal()
 
@@ -46,7 +50,10 @@ def listar_restaurantes():
     exibir_subtitulo('Listar restaurantes')
 
     for restaurante in restaurantes:
-        print(f'.{restaurante}')
+        nome_restaurante = restaurante['nome']
+        categoria = restaurante['categoria']
+        ativo = restaurante['ativo']
+        print(f'-{nome_restaurante} | {categoria} |{ativo}')
 
     voltar_ao_menu_principal()
 
